d4a66e5300e8c3e19a7287f1554d422e86c37ea8
burk-john <burkj@g.ucla.edu>
2022-05-10 22:18:03 -0700

    boilerplate code

--- kernelModel.py
+++ kernelModel.py
@@ -1,16 +1,26 @@
 import math
 
 # Titan V hardware characteristics
+FP32_MAX = 13.8 * 10e12 # 13.8TFLOPs max
+
 TOTAL_VRAM = 12884901888 # 12GB to bytes
 TOTAL_L2 = 4718592 # 4.5MB
-TOTAL_L1 = 98304 #96KB
+TOTAL_L1 = 98304 # 96KB
 
-VRAM_BW = 701153411072 #653GB/s
-L2_BW = 5
-L1_BW =
+VRAM_BW = 701153411072 # 653GB/s
+L2_BW = 1600 * (2**30) # 1600GB/s
+L1_BW = 3
 
 SM_COUNT = 80
 
-MEM_CLK = 850000000
+MEM_CLK = 850000000 # estimated memory clock speed
 GPU_CLK = 1200000000
-GPU_CLK_BOOST = 1455000000
+GPU_CLK_BOOST = 1455000000 # boost clock speed
+
+def calculate_compute_time():
+    return 0
+
+def calculate_memory_time():
+    return 0
+
+print('wtf')
