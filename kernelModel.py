a5aa891d511032f97f4e7b2f6a6ee83c4f88b0c7
burk-john <burkj@g.ucla.edu>
2022-05-14 18:28:32 -0700

    adding functionality for calculating compute and memory time

--- kernelModel.py
+++ kernelModel.py
@@ -8,8 +8,8 @@ TOTAL_L2 = 4718592 # 4.5MB
 TOTAL_L1 = 98304 # 96KB
 
 VRAM_BW = 701153411072 # 653GB/s
-L2_BW = 1600 * (2**30) # 1600GB/s
-L1_BW = 3
+L2_BW = 2155 * (2**30) # 2155GB/s. Slightly inaccurate, using V100 value
+L1_BW = 13800 * (2**30)
 
 SM_COUNT = 80
 
@@ -17,10 +17,30 @@ MEM_CLK = 850000000 # estimated memory clock speed
 GPU_CLK = 1200000000
 GPU_CLK_BOOST = 1455000000 # boost clock speed
 
+data_size = 4 # FP32, 32 bits, 8bits/byte
+
 def calculate_compute_time():
-    return 0
+    total_flops = 2*Ni*Nn*B # Add, multiply for problem size
+    return total_flops/FP32_MAX
 
 def calculate_memory_time():
     return 0
 
-print('wtf')
+def set_size(N_i, N_n, batch):
+    global Ni, Nn, B
+    Ni = N_i
+    Nn = N_n
+    B = batch
+
+def main():
+    #Classifier Stuff
+    if(class):
+        #Parameter to be tweaked
+        Ni = 2 ** 10    #Input size
+        Nn = 2 ** 10    #Output size
+
+        num_ops = Ni * Nn
+
+
+if __name__ == "__main__":
+    main()
